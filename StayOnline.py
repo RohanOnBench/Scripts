111c1ad6bcd40bdfcbd7a578cd7f6c797fd6e826
RohanOnBench <rohanonbench@gmail.com>
2024-07-05 04:37:18 +0530

    updated

--- StayOnline.py
+++ StayOnline.py
@@ -4,13 +4,26 @@ import random
 import time
 
 screen.FAILSAFE=False
-x,y=screen.size()
-while True:
-    # x1=random.randint(0,x)
-    # y1=random.randint(0,y)
-    # screen.moveTo(x1,y1)
-    time.sleep(10)
-    screen.press('space')
-    screen.hotkey('shift','3')
+def fetchTime(seconds):
+  hours = seconds // 3600
+  remaining_seconds = seconds % 3600
+  minutes = remaining_seconds // 60
+  seconds = remaining_seconds % 60
+  hours_str = f"{hours:02d}"
+  minutes_str = f"{minutes:02d}"
+  seconds_str = f"{seconds:02d}"
+  return f"{hours_str}:{minutes_str}:{seconds_str}"
+
+time_unit=10
+count=0
+message='Jai Shree Ram'
+try:
+    while True:
+        time.sleep(time_unit)
+        screen.typewrite(message)
+        screen.hotkey('enter')
+        count=count+1
+except KeyboardInterrupt:
+    print("\nTotal Count:"+str(count)+", Duration: "+fetchTime(count*time_unit))
 
     
