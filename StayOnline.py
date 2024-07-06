3b3fdc691a23b6a869768d87cdefabb12fbee0b4
RohanOnBench <rohanonbench@gmail.com>
2024-07-06 09:22:03 +0530

    latest code

--- StayOnline.py
+++ StayOnline.py
@@ -1,29 +1,31 @@
-# To install pyautogui use command 'pip install pyautogui'
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                  Please Follow Me Here                  #
+#  Youtube    :                                           #
+#  Instagram  :                                           #
+#  X(Twitter) :                                           #
+#  Facebook   :                                           #
+#                                                         #
+# To install pyautogui use command 'pip install pyautogui'#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 import pyautogui as screen
-import random
 import time
-
-screen.FAILSAFE=False
-def fetchTime(seconds):
-  hours = seconds // 3600
-  remaining_seconds = seconds % 3600
-  minutes = remaining_seconds // 60
-  seconds = remaining_seconds % 60
-  hours_str = f"{hours:02d}"
-  minutes_str = f"{minutes:02d}"
-  seconds_str = f"{seconds:02d}"
-  return f"{hours_str}:{minutes_str}:{seconds_str}"
 
 time_unit=10
 count=0
-message='Jai Shree Ram'
+message='|| Jai Shree Ram |'
+
+start=time.time()
 try:
     while True:
         time.sleep(time_unit)
-        screen.typewrite(message)
-        screen.hotkey('enter')
+        for c in message:
+            screen.press(c)
+        screen.press('|')
+        screen.press('enter')
         count=count+1
 except KeyboardInterrupt:
-    print("\nTotal Count:"+str(count)+", Duration: "+fetchTime(count*time_unit))
+    end=time.time()
+    duration=time.strftime('%H:%M:%S',time.gmtime(round(end-start)))
+    print("\nTotal Count: "+str(count)+", Duration: "+duration)
 
     
